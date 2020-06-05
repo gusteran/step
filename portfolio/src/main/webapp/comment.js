aaa190a26772a3a3d177dfd41ee85c2571302c7b
Gus Teran <ateran@wpi.edu>
2020-06-05 21:34:26 +0000

    Add pagination
    Add pagination to the comments with a footer of pages as well as a
    selector for the amount of comments to show. Minor visual tweaks. Move
    delete all comments button to hidden admin page

--- portfolio/src/main/webapp/comment.js
+++ portfolio/src/main/webapp/comment.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var commentID = 0;
+
 function addComment() {
     const commentContainer = document.getElementById('commentContainer');
     
@@ -19,15 +21,36 @@ function addComment() {
     commentContainer.value = "";
 }
 
-function fetchComments(numComments) {
+function fetchComments(numComments){
+    console.log("page 1");
+    fetchCommentsPerPage(numComments, 1);
+}
+
+function fetchCommentsPerPage(numComments, page) {
+    var totalComments = 0;
     document.getElementById('commentListContainer').innerHTML = "";
 	fetch('/comment').then(response => response.json()).then((comments) => {
-
         for(i in comments){
-            if(i < numComments){
+            // console.log(comments[i].text);
+            // console.log(i+" is between? "+(numComments * (page-1))+" and "+(numComments * page));
+            if(i < numComments * page && i >= numComments * (page-1)){
                 displayComment(comments[i]);
             }
+            totalComments = i;
+            // console.log(totalComments);
         }
+    var pageNum = 1;
+    var htmlStr = "Page ";
+    // console.log("Total Comment "+totalComments+" and page num "+pageNum);
+    document.getElementById('pageContainer').innerHTML ="";
+    while(totalComments > 0){
+        // console.log("Total Comment "+totalComments+" and page num "+pageNum);
+        htmlStr += (pageNum === page) ? "<span class='page_number selected'" : "<span class='page_number'";
+        htmlStr += " onclick='fetchCommentsPerPage("+numComments+","+pageNum+");'>"+pageNum+"</span>";
+        pageNum++;
+        totalComments-=numComments;
+    }
+    document.getElementById('pageContainer').innerHTML += htmlStr;
 	});
 }
 
@@ -36,7 +59,15 @@ function deleteComments() {
     fetchComments(10);
 }
 
+/*
+prevent user from writing html content by first creating the html to hold the comment
+then adding the parts of the comment as inner text to the html elements
+*/
 function displayComment(comment){
 		document.getElementById('commentListContainer').innerHTML += 
-            "<div class='comment'><span>"+comment.date+"</span><h3>"+comment.name+"</h3><p>"+comment.text+"</p></div>";
+            "<div class='comment'><span id='commentDate"+commentID+"'></span><h3 id='commentName"+commentID+"'></h3><p id='commentText"+commentID+"'></p></div>";
+        document.getElementById("commentDate"+commentID).innerText = comment.date;
+        document.getElementById("commentName"+commentID).innerText = comment.name;
+        document.getElementById("commentText"+commentID).innerText = comment.text;
+        commentID++;
 }
