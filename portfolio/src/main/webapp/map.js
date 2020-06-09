05399770c889087d7c2bdb818ad41c8895d6b6a6
Gus Teran <ateran@wpi.edu>
2020-06-09 23:37:35 +0000

    Add infowindow for markers
    Add infowindows that open on click for the marker. They contain the site
    name and the danger score. They close when the background map is clicked

--- portfolio/src/main/webapp/map.js
+++ portfolio/src/main/webapp/map.js
@@ -10,10 +10,21 @@ function initMap() {
 
 fetch('/superfund').then(response => response.json()).then((sites) => {
     sites.forEach((site) => {
-      new google.maps.Marker(
-          {position: {lat: site.lattitude, lng: site.longitude}, 
-          title: site.name,
-          map: map});
+      var marker = new google.maps.Marker(
+				{position: {lat: site.lattitude, lng: site.longitude}, 
+				title: site.name,
+				map: map});
+			var contentStr = "<h3>"+site.name+"</h3>" +
+				"<h4>Hazard Rating of: "+site.score+"</h4>";
+      var infoWindow = new google.maps.InfoWindow({
+				content: contentStr
+			})
+    	marker.addListener('click', () => {
+				infoWindow.open(map, marker);
+			});
+			map.addListener('click', () => {
+				infoWindow.close();
+			})
     });
   });
 }
